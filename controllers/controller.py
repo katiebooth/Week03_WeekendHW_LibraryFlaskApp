7a048e704b9317fae3c7602d87246df1a809a379
Katie Booth <katiebooth3@gmail.com>
2022-08-13 13:35:28 +0100

    Styled using CSS

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,11 +5,16 @@ from models.book_list import books
 
 @app.route('/')
 def start():
-    return "Hello World"
+    return "Hello, welcome to the library! Please add '/books' in the URL to go to the homepage"
 
 @app.route('/books')
-def library_index():
+def index():
     return render_template('index.html', title = "Library", books = books)
+
+@app.route('/books/<index>')
+def show(index):
+    book_to_show = books[int(index)]
+    return render_template('show.html',book=book_to_show)
 
 @app.route('/books/new')
 def new():
@@ -22,4 +27,19 @@ def create():
     genre = request.form['genre']
     new_book = Book(title,author,genre)
     books.append(new_book)
+    return redirect('/books')
+
+# @app.route('/books/remove')
+# def delete():
+#     return render_template('remove.html')
+
+@app.route('/books/remove/<title>', methods=['POST'])
+def remove(title):
+    # title = request.form['title']
+    book_to_delete = None
+    for book in books:
+        if book.title == title:
+            book_to_delete = book
+            break
+    books.remove(book_to_delete)
     return redirect('/books')
